be1b86e3ebc8332bb906aa032f58e73ce1179a3f
Xhulu <65667993+IANXHULU@users.noreply.github.com>
2024-03-06 13:01:06 +0200

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,5 @@
+def uInput(*arg, **kwargs):
+    Name = input(str())
+    Age = input(int())
+    location = input(str())
+    print("Hello  {}, your are  {}  years old and you live in {} ".format(Name, Age, location))
