eb25a7294b379e93f5de208d18bc924011a195b4
Xhulu <65667993+IANXHULU@users.noreply.github.com>
2024-04-01 09:14:49 +0200

    Update Dart Control Flow Assignment.dart

--- Dart Control Flow Assignment.dart
+++ Dart Control Flow Assignment.dart
@@ -4,10 +4,10 @@ void main() {
   int? n = int.parse(stdin.readLineSync()!);
 
   if (n < 10) {
-    print("The number is greater than 10");
+    print("The number is less than 10");
   } else if (n == 10) {
-    print("the number is equa to 10");
+    print("the number is equal to 10");
   } else {
-    print("the number is less than 10");
+    print("the number is greater than 10");
   }
 }
