db4ec7b1ab2b219385b472b806b03331048bf289
Xhulu <65667993+IANXHULU@users.noreply.github.com>
2024-04-02 07:25:27 +0200

    Create classPerson.dart

---
+++ OOP/classPerson.dart
@@ -0,0 +1,11 @@
+class Person {
+  String name;
+  String address;
+  int age;
+
+  Person(this.name, this.address, this.age);
+
+  void myPerson() {
+    print("My name is: $name, I live in : $address, and I am $age years old.");
+  }
+}
